964efed20ff3882444769bdd4d2d31a7f5a018d5
Kim-Daehee123 <57423919+Kim-Daehee123@users.noreply.github.com>
2021-03-18 15:44:17 +0900

    Update EmployeeManager.kt

--- src/EmployeeManager.kt
+++ src/EmployeeManager.kt
@@ -1,6 +1,5 @@
 package employee
 
-import java.text.NumberFormat
 import java.util.*
 
 fun main() {
@@ -21,20 +20,24 @@ class EmployeeManager {
         println("2: 사원 추가")
         println("3: 사원 총 월급 조회")
         println("4: 사원 평균 월급 조회")
+        println("5: 부서 총 월급 조회")
+        println("6: 부서 평균 월급 조회")
         println("7: 종료")
     }
     //사원 관리 메뉴
     fun start(){
         var menuNumber = 0
 
-        while(menuNumber != 3) {
+        while(menuNumber != 7) {
             managementMenu()
             menuNumber = readLine()?.toInt()!!
+            
             when(menuNumber) {
                 1->lookupEmployees()
                 2->addEmployee()
                 3->totalsalaryEmployee()
                 4->averagesalaryEmployee()
+                5->totalsalaryDepartment()
                 7->break
                 else->{}
             }
@@ -42,14 +45,15 @@ class EmployeeManager {
     }
 
     //사원 목록 조회
-    fun lookupEmployees() {
+    private fun lookupEmployees() {
         println("=====모든 사원의 정보를 출력합니다=====")
+
         for (i in employees) {
             println("사원명 : ${i.employeeName}")
             println("식별번호 : ${i.employeeUUID}")
             println("부서 : ${i.department.departmentName}")
             println("계약형태 : ${i.employeecontract}")
-            println("월급 : ${i.employeeSalary}")
+            println("월급 : ${i.employeeSalary}원")
             println("=====================")
         }
     }
@@ -67,6 +71,7 @@ class EmployeeManager {
 
         var departmentNumber = readLine()?.toInt()!!
         lateinit var department : Department
+
         when(departmentNumber) {
             1 -> department = Department.Dev
             2 -> department = Department.Sales
@@ -82,6 +87,7 @@ class EmployeeManager {
 
         var contractNumber = readLine()?.toInt()!!
         lateinit var employee : Employee
+
         when(contractNumber) {
             1 -> {
                 //계약직
@@ -112,7 +118,7 @@ class EmployeeManager {
         println("식별번호 : ${employee.employeeUUID}")
         println("부서 : ${employee.department.departmentName}")
         println("계약형태 : ${employee.employeecontract}")
-        println("월급 : ${employee.employeeSalary}")
+        println("월급 : ${employee.employeeSalary}원")
         employees.add(employee)
     }
     //사원들의 총 월급
@@ -122,16 +128,64 @@ class EmployeeManager {
         for (i in employees) {
             salary += i.employeeSalary
         }
-        println("사원 총 월급 : ${salary}만원")
+        println("사원 총 월급 : ${salary}원")
     }
     //사원의 평균 월급
     private fun averagesalaryEmployee() {
-        println("=======사원 총 월급=======")
+        println("=======사원 평균 월급=======")
         var salary : Long = 0
         for (i in employees) {
             salary += i.employeeSalary
         }
         salary/employees.size
-        println("사원 총 월급 : ${salary}원")
+        println("사원 평균 월급 : ${salary}원")
+    }
+    //부서별 총 월급
+    private fun totalsalaryDepartment() {
+        println("조회할 부서를 지정해주세요")
+        println("1: 개발팀")
+        println("2: 영업팀")
+        println("3: 고객대응팀")
+        println("4: 사무직")
+
+        var salary : Long = 0
+        var lookupNumber = readLine()?.toInt()!!
+
+        when(lookupNumber){
+            1 -> {
+                //개발팀
+                for (i in employees) {
+                    if(i.department == Department.Dev) {
+                        salary+=i.employeeSalary
+                    }
+                }
+            }
+            2 -> {
+                //영업팀
+                for (i in employees) {
+                    if(i.department == Department.Sales) {
+                        salary+=i.employeeSalary
+                    }
+                }
+            }
+            3 -> {
+                //고객대응팀
+                for (i in employees) {
+                    if(i.department == Department.Cs) {
+                        salary+=i.employeeSalary
+                    }
+                }
+            }
+            4 -> {
+                //사무직
+                for (i in employees) {
+                    if(i.department == Department.Office) {
+                        salary+=i.employeeSalary
+                    }
+                }
+            }
+        }
+        println("=======부서 총 월급=======")
+        println("부서 총 월급 : ${salary}원")
     }
 }
