d7a71faf99e41ab4cdfb6b1eaa7a8ce056b7301c
천수빈 <suebin710@gmail.com>
2021-03-18 09:14:50 +0900

    add remark

--- InheritanceReal/src/Employee.kt
+++ InheritanceReal/src/Employee.kt
@@ -3,10 +3,12 @@ import java.util.UUID
 open class Employee(val name: String, val id: UUID, val department: Department) {
     open fun calculateSalary() = 0
 
+    // 연봉 / 시급 정보 상수처리
     companion object {
         const val BASIC_YEARLY_SALARY = 120000000
-        const val BASIC_DAILY_SALARY = 25000
+        const val BASIC_HOURLY_SALARY = 25000
     }
+
 }
 
 class PermanentEmployee(name: String, id: UUID, department: Department) :
@@ -21,8 +23,9 @@ class PartTimeEmployee(
     private val workingHour: Int,
     private val workingDate: Int
 ) : Employee(name, id, department) {
-    override fun calculateSalary(): Int = BASIC_DAILY_SALARY.times(workingHour).times(workingDate)
+    override fun calculateSalary(): Int = BASIC_HOURLY_SALARY.times(workingHour).times(workingDate)
 }
+
 
 class SalesEmployee(
     name: String,
